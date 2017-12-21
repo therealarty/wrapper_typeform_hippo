61eecf1599045ecf920a8c6bd2061c5dbe267cf4
hippo26 <hipothebest26@hotmail.fr>
2017-12-21 16:17:42 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='wrapper_typeform',
+      version='0.1',
+      description=' Convert Data from Typeform API to two lists (questions and answers) written in Python',
+      url='https://github.com/Hippo26/wrapper_typeform-python',
+      author='Hippolyte Hazard',
+      author_email='hippolyte.hazard@gmail.com',
+      license='GPL',
+      packages=['wrapper_typeform'],
+      install_requires=[
+          'requests', 'pandas','uuid'
+      ],
+      zip_safe=False)
