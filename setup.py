fdabd8670a7fd944e61d33e87eb9fca43f0f2bdc
hippo26 <hipothebest26@hotmail.fr>
2017-12-21 17:25:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='wrapper_typeform_hippo',
+setup(name='wrapper_typeform',
       version='0.1',
       description=' Convert Data from Typeform API to two lists (questions and answers) written in Python',
       url='https://github.com/Hippo26/wrapper_typeform-python',
